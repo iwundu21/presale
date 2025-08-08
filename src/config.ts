50147035fed2d9346072480d839583b7bf2d19d4
Mike Ebube <mikeebube14@gmail.com>
2025-08-08 15:37:28 +0000

    Now let replace the receiver wallet,   5Gy5qYXhYs7aPfEztAG6vTPVow5snudPk

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 
 import { PublicKey } from "@solana/web3.js";
 
-export const PRESALE_WALLET_ADDRESS = "9Kqt28pfMVBsBvXYYnYQCT2BZyorAwzbR6dUmgQfsZYW";
+export const PRESALE_WALLET_ADDRESS = "5Gy5qYXhYs7aPfEztAG6vTPVow5snudPksBvF5DAYLpX";
 export const RPC_URL = "https://mainnet.helius-rpc.com/?api-key=22d7510e-d9de-4988-bd33-b4338041e0d0";
 
 export const USDC_MINT = new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"); // Mainnet
@@ -10,4 +10,3 @@ export const USDT_MINT = new PublicKey("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8Ben
 export const EXN_PRICE = 0.09;
 export const SOFT_CAP = 500_000_000;
 export const HARD_CAP = 700_000_000;
-
