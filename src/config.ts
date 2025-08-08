41ca187a6da3ab58f8748a22ad89401fffb6af3b
Mike Ebube <mikeebube14@gmail.com>
2025-08-08 14:32:54 +0000

    https://mainnet.helius-rpc.com/?api-key=22d7510e-d9de-4988-bd33-b4338041

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 
 export const PRESALE_WALLET_ADDRESS = "9Kqt28pfMVBsBvXYYnYQCT2BZyorAwzbR6dUmgQfsZYW";
-export const RPC_URL = "https://mainnet.helius-rpc.com/?api-key=YOUR_API_KEY_HERE";
+export const RPC_URL = "https://mainnet.helius-rpc.com/?api-key=22d7510e-d9de-4988-bd33-b4338041e0d0";
