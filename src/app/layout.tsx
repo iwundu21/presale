75c14ec49e72d6c380e277e9ef7a806f9bbf4142
Mike Ebube <mikeebube14@gmail.com>
2025-08-09 06:40:19 +0000

    It still showing unknown

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <title>Exnus Presale</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
