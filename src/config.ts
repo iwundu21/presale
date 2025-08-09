a08091fe2267307ac9c5c2e9fd1b1b631ac43af4
Mike Ebube <mikeebube14@gmail.com>
2025-08-09 06:28:25 +0000

    https://mainnet.helius-rpc.com/?api-key=ef62b792-793e-4ecd-a102-d7cb7f1c

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const PRESALE_WALLET_ADDRESS = "5Gy5qYXhYs7aPfEztAG6vTPVow5snudPksBvF5DAYLpX";
-export const RPC_URL = `https://mainnet.helius-rpc.com/?api-key=${process.env.NEXT_PUBLIC_HELIUS_API_KEY}`;
+export const RPC_URL = "https://mainnet.helius-rpc.com/?api-key=ef62b792-793e-4ecd-a102-d7cb7f1c023f";
 
 export const USDC_MINT = new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"); // Mainnet
 export const USDT_MINT = new PublicKey("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB"); // Mainnet
@@ -10,6 +10,7 @@ export const USDT_MINT = new PublicKey("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8Ben
 export const EXN_PRICE = 0.09;
 export const SOFT_CAP = 500_000_000;
 export const HARD_CAP = 700_000_000;
+
 
 
 
