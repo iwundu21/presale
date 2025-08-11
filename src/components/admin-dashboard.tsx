272641014fa32cc350d3d7f28daf967a3ec84d22
Mike Ebube <mikeebube14@gmail.com>
2025-08-11 11:41:30 +0000

    ReferenceError: React is not defined     at https://9000-firebase-studio

--- src/components/admin-dashboard.tsx
+++ src/components/admin-dashboard.tsx
@@ -1,6 +1,7 @@
 
 "use client";
 
+import * as React from "react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
