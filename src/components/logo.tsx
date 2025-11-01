f0d32da32751500127e61abc9909ef9f5b9f87c1
Mike Ebube <mikeebube14@gmail.com>
2025-11-01 06:51:26 +0000

    Please  reduce  the size  of the  writing  and make it  match  in single

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,5 +3,5 @@ export function Logo({ isAdmin = false }: { isAdmin?: boolean }) {
     if (isAdmin) {
         return <h1 className="text-2xl font-bold text-white">Exnus Admin</h1>
     }
-    return <h1 className="text-2xl font-bold text-white">Exnus protocol</h1>
+    return <h1 className="text-xl font-bold text-white">Exnus protocol</h1>
 }
